c6ce97ee6ac33b9288afaeab4f30ca8034f7718b
Ricardo Melida <ricardomelida92@gmail.com>
2023-11-17 16:46:04 -0300

    Se argega dbcontext oracle

--- Api.IBPR.WebSite.Infrastructure/Api.IBPR.Website.Persistence/ServiceExtensions.cs
+++ Api.IBPR.WebSite.Infrastructure/Api.IBPR.Website.Persistence/ServiceExtensions.cs
@@ -1,15 +1,17 @@
+using Api.IBPR.Website.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Api.IBPR.Website.Infrastructure.Api.IBPR.Website.Persistence
 {
-    public static class ServiceExtension
+    public static class ServiceExtensions
     {
         public static void ConfigurePersistence(this IServiceCollection services, IConfiguration configuration)
         {
-            var connectionString = configuration.GetConnectionString("PostgresConnection");
-            services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(connectionString));
+            var connectionString = configuration.GetConnectionString("OracleConnection");
+            services.AddDbContext<OracleDbContext>(opt => opt.UseOracle(connectionString));
+            //services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(connectionString));
 
             //services.AddScoped<IUnitOfWork, UnitOfWork>();
             //services.AddScoped<IUserRepository, UserRepository>();
--- Api.IBPR.Website.Presentation/Api.IBPR.Website.WebAPI/Program.cs
+++ Api.IBPR.Website.Presentation/Api.IBPR.Website.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using Api.IBPR.Website.Infrastructure.Api.IBPR.Website.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +9,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.ConfigurePersistence(builder.Configuration);
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
